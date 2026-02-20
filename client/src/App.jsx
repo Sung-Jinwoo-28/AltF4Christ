cf8231c4cfe11c91c517df228113e74c1859588d
Sung-Jinwoo-28 <rmodi182@gmail.com>
2026-02-20 17:13:43 +0530

    feat(ui): add lenis smooth scrolling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import Lenis from 'lenis';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -11,6 +13,31 @@ import ResourceDetail from './pages/ResourceDetail';
 import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <AuthProvider>
       <Router>
